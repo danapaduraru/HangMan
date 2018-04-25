d2c475564e48fa1188680c49aec5036ab797240a
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2018-04-25 10:23:54 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <graphics.h>
+#include "Word.h"
+#include "Graphics.h"
+using namespace std;
+
+int main()
+{
+    Word myWord;
+	myWord.GraphicsStartGame();
+    return 0;
+}
